d499e23eaa14016399dd0f9c8c92b3fcd2897a72
qwertxfz <hohoko555888@gmail.com>
2024-11-26 19:35:02 +0200

    Initial commit

---
+++ ConsoleApp1/Model/ListSort.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace test
+{
+    public class ListSort
+    {
+        List<int> arr = new List<int>() { 1, 6, 9, 7, 8, 1, 3, 5, 94 };
+
+        public void SortByDescending()
+        {
+            arr.Sort((x, y) => y.CompareTo(x);
+            Console.WriteLine(string.Join(" ", arr));
+        }
+
+        public void ScreenList()
+        {
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
+
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApp1;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            ListSort listSort = new ListSort();
+            listSort.ScreenList();
+            listSort.SortByDescending();
+        }
+    }
+}
